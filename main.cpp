6f9a1583db4f4688724990612cdabdaf6dee03c9
Theo Patron <patron.theo@gmail.com>
2021-12-08 15:20:01 +0100

    parsing command line agrument clean

--- main.cpp
+++ main.cpp
@@ -3,8 +3,7 @@
 #include <fstream>
 #include <vector>
 #include <Eigen/Dense>
-// #include <cstring>
-#include <stdlib.h>     /* atoi */
+#include <stdlib.h>     /* atof */
 #include "getopt.h"
 
 #include "AbstractLinalgSolver.hpp"
@@ -17,7 +16,6 @@
 
 using namespace Eigen;
 
-// static int verbose_flag;
 
 int main(int argc, char *argv[])
 {   
@@ -30,19 +28,8 @@ int main(int argc, char *argv[])
         {
         static struct option long_options[] =
             {
-            /* These options set a flag. */
-            // {"verbose", no_argument,       &verbose_flag, 1},
-            // {"brief",   no_argument,       &verbose_flag, 0},
             {"method",   required_argument,       0, 'm'},
             {"shift",   required_argument,       0, 's'}
-            /* These options don’t set a flag.
-                We distinguish them by their indices. */
-            // {"add",     no_argument,       0, 'a'},
-            // {"append",  no_argument,       0, 'b'},
-            // {"delete",  required_argument, 0, 'd'},
-            // {"create",  required_argument, 0, 'c'},
-            // {"file",    required_argument, 0, 'f'},
-            // {0, 0, 0, 0}
             };
         /* getopt_long stores the option index here. */
         int option_index = 0;
@@ -56,56 +43,20 @@ int main(int argc, char *argv[])
 
         switch (c)
             {
-            case 0:
-            /* If this option set a flag, do nothing else now. */
-            if (long_options[option_index].flag != 0)
-                break;
-            printf ("option %s", long_options[option_index].name);
-            if (optarg)
-                printf (" with arg %s", optarg);
-            printf ("\n");
-            break;
-
-            // case 'a':
-            // puts ("option -a\n");
-            // break;
-
-            // case 'b':
-            // puts ("option -b\n");
-            // break;
-
-            // case 'c':
-            // printf ("option -c with value `%s'\n", optarg);
-            // break;
-
-            // case 'd':
-            // printf ("option -d with value `%s'\n", optarg);
-            // break;
-
             case 's':
-            shift = atof(optarg);
+                shift = atof(optarg);
             break;
 
             case 'm':
-            if(strcmp(optarg, "power") == 0) method = 0;
-            if(strcmp(optarg, "inversepower") == 0) method = 1;
-            if(strcmp(optarg, "QR") == 0) method = 2;
-            break;
-
-            case '?':
-            /* getopt_long already printed an error message. */
+                if(strcmp(optarg, "power") == 0) method = 0;
+                if(strcmp(optarg, "inversepower") == 0) method = 1;
+                if(strcmp(optarg, "QR") == 0) method = 2;
             break;
 
             default:
-            abort ();
+                abort ();
             }
         }
-
-    /* Instead of reporting ‘--verbose’
-        and ‘--brief’ as they are encountered,
-        we report the final status resulting from them. */
-    // if (verbose_flag)
-    //     puts ("verbose flag is set");
 
     /* Print any remaining command line arguments (not options). */
     if (optind < argc)
