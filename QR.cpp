a4e48c27c7f7e0f0779cc501076488a57507d2f3
charlottesertic <charlotte.sertic@epfl.ch>
2021-12-12 17:52:11 +0100

    conventions

--- QR.cpp
+++ QR.cpp
@@ -21,37 +21,37 @@ QR<T1,T2,T3>::~QR() {}
 template <typename T1,typename T2,typename T3>
 T2 QR<T1,T2,T3>::SolveEquation() {
     //Get members 
-    T1 A = this->mMatrix;
+    T1 a = this->mMatrix;
     double error =this->mError;
 
-    int n = A.rows();
+    int dim = A.rows();
     //Initialise iterative variables 
-    T1 A_new(n,n);
-    T1 Q(n,n);
-    T1 R(n,n);
+    T1 a_new(dim,dim);
+    T1 Q(dim,dim);
+    T1 R(dim,dim);
 
     T2 e = T2::Random(n);
-    T2 d = A.diagonal();
-    T2 d_new;
+    T2 diag = A.diagonal();
+    T2 diag_new;
 
     HouseholderQR<T1> qr(n,n);
 
     while(e.norm()>error) {
     //compute QkRk= A(k-1)
-    qr.compute(A);
-    Q=qr.householderQ();
-    R=Q.transpose()*A;
+    qr.compute(a);
+    q=qr.householderQ();
+    r=Q.transpose()*a;
     //Reassemble factors Ak=RkQk
-    A_new = R*Q;
-    A=A_new;
-    d_new = A.diagonal();
+    a_new = r*q;
+    a=a_new;
+    diag_new = a.diagonal();
     //calculate error
-    e = d-d_new;
-    d=d_new;
+    e = diag-diag_new;
+    diag=diag_new;
     
     }   
-    std::cout<<A<<std::endl;;
-    return d;
+
+    return diag;
 
 }   
 
